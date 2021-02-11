42625266e2ea761a575469026bdf1aab733dde24
Rana Faizan <rana.faizan@66loop.com>
2021-02-11 18:24:37 +0500

    Initial Commit

---
+++ controllers/authentication/userController.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const models = require('../../models');
+
+exports.register = function(req,res,next){
+    const user = {
+        firstName:req.body.firstname,
+        lastName:req.body.lastname,
+        country:req.body.country,
+        city:req.body.city,
+        email:req.body.email,
+        password:req.body.password,
+        userStatusId:req.body.userStatus
+    }
+    
+    models.User.create(user).then(result => {
+        res.status(201).json({
+            message: "User Created Successfully",
+            post: result  
+        });
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong",
+            error: result  
+        });
+    });
+
+};
---
+++ migrations/20210211125043-create-user.js
@@ -0,0 +1,84 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      userStatusId: {
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      uniqueIdentifier: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      addressLine1: {
+        type: Sequelize.STRING
+      },
+      addressLine2: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      zipCode: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      recommendedItems: {
+        type: Sequelize.STRING
+      },
+      recentlyViewedItems: {
+        type: Sequelize.STRING
+      },
+      creationDate: {
+        type: Sequelize.DATE
+      },
+      lastUpdatedDate: {
+        type: Sequelize.DATE
+      },
+      isActive: {
+        type: Sequelize.CHAR
+      },
+      isDeleted: {
+        type: Sequelize.CHAR
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
